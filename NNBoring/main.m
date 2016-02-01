f496ce7623ab52a00d2df44a6ed38d558f850ee3
fish <fish@fishdemacbook-pro.local>
2016-02-01 23:26:22 +0800

    Initial Commit

---
+++ NNBoring/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  NNBoring
+//
+//  Created by fish on 16/2/1.
+//  Copyright © 2016年 Noname. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
